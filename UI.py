086df5070e34a9e8adaf5428f4302fe610afea56
anna-marie-vos <amvos@outlook.com>
2018-08-11 22:15:12 +1200

    adding timing

--- UI.py
+++ UI.py
@@ -14,12 +14,18 @@ class UI:
         self.imagePath = ''
         self.counter = 0
         self.imageList=['item','item2','item3']
+        # self.timeit = False
+        # self.interval = 5000
 
         self.nextBtn = Button(self.window, text='back', width=6, command=lambda: self.goToPreviousImage("back"))
         self.nextBtn.pack(side=LEFT)
 
         self.getFolderBtn = Button(self.window, text = "Select a Folder", width = 12, command=self.getFolder)
         self.getFolderBtn.pack()
+
+        # self.thirtySecTimer = Button(self.window, text='30 seconds', width=10, command=lambda: self.setCountdown(2))
+        # self.thirtySecTimer.pack(side=BOTTOM)
+
 
         self.nextBtn = Button(self.window, text='next', width=6, command=lambda: self.goToNextImage("next"))
         self.nextBtn.pack(side=RIGHT)
@@ -29,7 +35,6 @@ class UI:
         filename = filedialog.askdirectory()
         self.folderPath.set(filename)
         self.imageList = os.listdir(str(self.folderPath.get()))
-        print(self.imageList[0])
         self.openImage()
 
     def openImage(self):
@@ -61,7 +66,6 @@ class UI:
         elif self.counter == len(self.imageList)-1:
             self.counter = len(self.imageList)-1
             self.openImage()
-        print('counter:',self.counter, 'length:',len(self.imageList), 'type:',type(self.img))
 
     def goToPreviousImage (self, btnPress):
         self.buttonPress = btnPress
@@ -75,6 +79,18 @@ class UI:
         elif self.counter == 0:
             self.counter = 0
             self.openImage()
+
+    # def timeLoop(self):
+    #     if self.timeit:
+    #         self.window.after(self.interval, self.timeLoop)
+    #
+    # def getInterval(self):
+    #     if self.timeInput.get() =="":
+    #         self.interval = 5000
+    #     else :
+    #         self.interval = self.timeInput.get()*1000
+
+
 
 
 root = Tk()
